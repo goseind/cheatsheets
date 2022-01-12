15a44b4c40f15f3beb0ef6a09d5f51f1b047950d
otiofrui <spam@dev.otiofrui.org>
2022-01-12 21:26:20 +0100

    update gdb

--- gdb.sh
+++ gdb.sh
@@ -1,3 +1,31 @@
 # gdb cheatsheet
 
+# CTRL-D: Beendet den Debugger
+# Stop prgramm with Ctrl+C
+
 info functions # show all 
+
+run/r # start program inside gdb
+backtrace/bt # show stack
+frame NO # jump to frame number
+disassemble/disas # disassemble function at current stack frame
+break/b # Breakpoint. Hierbei können Sie angeben
+break strcmp
+break *0x7ffff7f39660
+continue/c # continue program
+
+x # display memory contents at given address using the specified format
+x /x $rdi or x /x 0x555555559ac0 # hex
+x /d $rdi or x /d 0x555555559ac0 or x /u $rdi # dez
+x /f $rdi or x /f 0x555555559ac0 # float
+x /s $rdi or x /s 0x555555559ac0 # string
+x /i $rip or x /i 0x555555559ac0 # instruction
+x /o $rdi # octal
+x /t $rdi # binary
+x /a $rdi # address
+x /c $rdi # ascii
+x /10x $rdi # several values at once
+
+print/p # Register
+p $rdi # register
+p 22 # expression
