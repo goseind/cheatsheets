292283cf2babc651373f892b196ffbde8af2c49c
Domenic Gosein <87760323+otiofrui@users.noreply.github.com>
2022-01-13 12:40:14 +0100

    Update kubernetes_cs.sh

--- kubernetes_cs.sh
+++ kubernetes_cs.sh
@@ -1,12 +1,22 @@
 # kubernetes cheatsheet
 
-# kubectl: https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+# kubectl reference: https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+
 # autocomplete
 source <(kubectl completion bash)
 echo "source <(kubectl completion bash)" >> ~/.bashrc
+
 # alias
 alias k=kubectl
 complete -F __start_kubectl k
 
+# kubectl config
+kubectl config current-context
+kubectl config get-contexts
+kubectl config use-context XY
+
 # Krew plugins: https://krew.sigs.k8s.io/
 kubectl krew install deprecations
+
+## deprecations
+kubectl deprecations
