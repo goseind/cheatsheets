c7f4b5db44d3f014740361417465b14c527d3d7a
Domenic Gosein <87760323+otiofrui@users.noreply.github.com>
2022-01-13 11:39:29 +0100

    Update kubernetes_cs.sh

--- kubernetes_cs.sh
+++ kubernetes_cs.sh
@@ -1,1 +1,10 @@
 # kubernetes cheatsheet
+
+# kubectl: https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+# autocomplete
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+# alias
+alias k=kubectl
+complete -F __start_kubectl k
+
