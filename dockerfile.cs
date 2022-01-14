91bf5dad3d513796f2b7b1deee5fde7a8ec238d0
Domenic Gosein <87760323+otiofrui@users.noreply.github.com>
2022-01-14 23:54:09 +0100

    Update dockerfile.cs

--- dockerfile.cs
+++ dockerfile.cs
@@ -2,3 +2,12 @@
 
 # run interactive ubuntu bash
 docker run -i -t ubuntu /bin/bash
+
+# list images or containers
+sudo docker ps # all -a # only ids -q
+sudo docker images
+
+# delete all images and containers
+sudo docker rm -f id
+sudo docker rmi -f $(docker images -aq)
+sudo docker rm -f $(docker ps -aq)
